48cd96e99f0b8e5f9686661beddfbf0bb9e16ba1
amankunaljha <amankunaljha@gmail.com>
2024-11-20 13:38:47 +0000

    conversion in data type

---
+++ .devcontainer/01_basic/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3)// we are using nodejs,not browser
+
+let name = Aman
+let age =18
+let isloggedin = false
+
+
+// number=>2 to pwer 53
+//bigint
+//string=>""
+//boolean =>true/false
+//null => standalone value
+// undefined =>
+//symbol =>unique
+
+
+//object
+console.log(typeof age)
+
+
+
+
